04ac4d7650402c10900d5a4b0834d689f7d1cfd0
Quan Tran <anhquan0412@gmail.com>
2016-11-16 21:12:41 +0000

    add show page and new page for project. Need work on new page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,52 @@
 class ProjectsController < ApplicationController
+   before_action :set_project, only: [:edit, :update, :show, :like, :destroy]
+   
    def index
        @projects= Project.all
    end
     
+   
+   def new
+      @project = Project.new
+   end
+  
+   def create
+     @project = Project.new(project_params)
+     if @project.save
+        flash[:success] = "Your project was created successfully!" #this message is defined in _message.html.erb
+         redirect_to projects_path # REDIRECT_TO IS USED TO DIRECT TO A PATH/URL LINK
+      #let's NOT use render here....
+      
+      else
+         render :new #or render 'new'. RENDER IS USED TO DIRECT TO A HTML VIEW
+         #redirect_to new_project_path #use redirect_to will wipe out all your input
+      end
+   end
+   
+   
+   
+  
+   def show
+       @this_user = @project.user
+   end
+    
+   def edit
+   
+   end
+   
+   def update
+   
+   end
+   
+   
+   def destroy
+      
+   end
+   
+   
+   private
+      def set_project
+         @project = Project.find(params[:id])
+      end
+      
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
     validates :name, presence: true,
-                length: {minimum: 1, maximum: 50}
+                length: {minimum: 1, maximum: 100}
     
     validates :description, presence: true,
                 length: {maximum: 5000}
