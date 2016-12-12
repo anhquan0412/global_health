9be964a883390128b02bdc2f6f97ed5d906288f2
Quan Tran <anhquan0412@gmail.com>
2016-12-12 06:05:28 +0000

    create shared show form, add html for specialties

---
+++ app/controllers/specialties_controller.rb
@@ -0,0 +1,68 @@
+class SpecialtiesController < ApplicationController
+    
+    before_action :admin_user, only: [:new,:create,:edit, :update, :destroy]
+    before_action :set_specialty, except: [:new,:create]
+    
+    def new
+        @specialty = Specialty.new
+    end
+    
+    
+    
+    def create
+        @specialty = Specialty.new(specialty_params)
+        if @specialty.save
+          flash[:success] = "Your specialty has been created successfully"
+          redirect_to projects_path
+          
+        else
+          render :new
+        end
+    end
+  
+  def show
+   
+   @projects = @specialty.projects.paginate(page: params[:page], per_page: 3)
+  end
+  
+  
+  def edit
+    
+    
+  
+  end
+  
+  def update
+    
+    
+    if @specialty.update(specialty_params)
+      flash[:success] = "Specialty has been edited successfully"
+      redirect_to projects_path
+      
+    else
+      render :edit
+    end
+    
+    
+  end
+  
+  
+  def destroy
+    flash[:success] = " \"#{@specialty.name}\" is deleted successfully"
+    Specialty.find(params[:id]).destroy
+    
+    redirect_to projects_path
+    
+  end
+  
+  private
+      def specialty_params
+        params.require(:specialty).permit(:name)
+      end
+      
+      def set_specialty
+        @specialty = Specialty.find(params[:id])
+      end
+    
+end
+    
