17d7daeecdbc2f295bca4b9a38ed783584414b8e
bshap93 <bshap93@gmail.com>
2017-02-11 10:45:21 -0600

    Change labels instructing how to add multiple options

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
 
-    config.site_key = '6LcdohEUAAAAAAqWhCKFIrgDkSvP1Hls4eJVpxAG'
-    config.secret_key = '6LcdohEUAAAAANxUmWVSTP75vbnTQ0Xif0RoK_J9'
+    config.site_key = '6LdcrhEUAAAAALee5k-9AdPcMBft9VuuHPnbj_WQ'
+    config.secret_key = '6LdcrhEUAAAAAOwD3ynN35x5SxrpZZ01qfMWYuKx'
 
 end
