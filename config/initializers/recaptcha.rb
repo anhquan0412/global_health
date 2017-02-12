89a64aa410aa37bd905cad84c68ec6b4135adf70
Quan Tran <anhquan0412@gmail.com>
2017-02-12 06:30:11 +0000

    set up recaptcha key for production

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,10 @@
 Recaptcha.configure do |config|
-
-    config.site_key = '6LdcrhEUAAAAALee5k-9AdPcMBft9VuuHPnbj_WQ'
-    config.secret_key = '6LdcrhEUAAAAAOwD3ynN35x5SxrpZZ01qfMWYuKx'
+    if Rails.env.production?
+        config.site_key = ENV['RECAPT_SITE_KEY']
+        config.secret_key = ENV['RECAPT_SECRET_KEY']
+    else
+        config.site_key = '6LcdohEUAAAAAAqWhCKFIrgDkSvP1Hls4eJVpxAG' # or put your own recaptcha key here		  
+        config.secret_key = '6LcdohEUAAAAANxUmWVSTP75vbnTQ0Xif0RoK_J9' # same
+    end
 
 end
