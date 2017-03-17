6e599a5c08c372887ceac0c131b0cdf1162b5ec0
Quan Tran <anhquan0412@gmail.com>
2017-03-17 21:47:02 +0000

    minor fix, prepare to merge

--- config/initializers/ransack.rb
+++
@@ -1,7 +0,0 @@
-Ransack.configure do |config|
-  config.add_predicate 'has_institutions',
-    :arel_predicate => 'in',
-    :formatter => proc {|institution_ids| User.institution_subquery(institution_ids)},
-    :validator => proc {|v| v.present?},
-    :compounds => true
-end
